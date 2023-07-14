dd2a165b6332b730fec25a4ca668eb5cfab20ddd
Maxime Chambon <max.chambon@protonmail.ch>
2023-07-14 09:20:29 +0200

    login ok : redirect + features

---
+++ FrontEnd/assets/pages/login.js
@@ -0,0 +1,55 @@
+/* LOGIN */
+
+// Déclaration de l'url pour la requête fetch
+const url = "http://localhost:5678/api/users/login";
+
+// Déclaration du formulaire
+const loginForm = document.querySelector("form");
+// On écoute l'évenement lors de l'envoi des identifiants
+loginForm.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+    // On empêche le refresh de la page par défaut
+    event.preventDefault();
+
+    // Déclaration des valeurs à lier à l'objet
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector(".password").value;
+
+    // Déclaration de l'objet à envoyer
+    const formData = {
+        email: email,
+        password: password
+    };
+
+    // Déclaration de la valeur de l'option body 
+    const formJSON = JSON.stringify(formData);
+    console.log(formJSON);
+
+    // Requête fetch méthode POST
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: formJSON,
+    })
+        // Traitement de la réponse
+        .then(response => response.json())
+        .then(data => {
+            // Affichage des données retournées dans la console
+            console.log(data);
+            let token = data.token;
+            if (data.token) {
+                // Stockage du token en local 
+                localStorage.setItem("authToken", data.token)
+                // Renvoi à la page d'accueil
+                window.location.href = "../../index.html";
+            } else {
+                // Message d'erreur si mauvais identifiants
+                alert("Les information saisies sont incorrectes.");
+            }
+        })
+}
+
+console.log("Je suis sur la page de Login");
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,48 +1,127 @@
 /* PROJETS */
 
+//Appel à l'API pour récuperer les projets
+fetch("http://localhost:5678/api/works")
+    .then(reponse => reponse.json())
+    .then(data => {
+        console.table(data);
+
+        recupPortfolio(data);
+    })
+    .catch(error => {
+        console.error('Erreur lors de la récupération des données :', error);
+    });
+
 //Déclaration du conteneur des projets
-const gallery = document.querySelector(".gallery")
+const gallery = document.querySelector(".gallery");
 
 function recupPortfolio(data) {
 
     for (let i = 0; i < data.length; i++) {
-//Création fiche + image + sous-titre et lien avec contenu dynamique 
-        const projet = document.createElement("figure")
-        projet.setAttribute("id", data[i].category.name)
-        const image = document.createElement("img")
-        image.src = data[i].imageUrl
-        const subtitle = document.createElement("figcaption")
-        subtitle.innerHTML = data[i].title
-//Association Contenu - Parents 
-        gallery.appendChild(projet)
-        projet.appendChild(image)
-        projet.appendChild(subtitle)
+        //Création d'un article et apport du contenu dynamique
+        const projet = document.createElement("figure");
+        // Attribution d'une class
+        projet.setAttribute("class", "figure");
+        // Attribution d'un ID
+        projet.setAttribute("id", data[i].categoryId);
+        //Ajout d'une image
+        const image = document.createElement("img");
+        image.src = data[i].imageUrl;
+        //Ajout d'un sous-titre
+        const subtitle = document.createElement("figcaption");
+        subtitle.innerText = data[i].title;
+        //Association Contenu-Parents
+        gallery.appendChild(projet);
+        projet.appendChild(image);
+        projet.appendChild(subtitle);
     }
-}
 
-//Je fais l'appel à l'API pour récuperer les projets
-fetch("http://localhost:5678/api/works")
-    .then(reponse => reponse.json())
-    .then(data => {
-        console.table(data)
+    // Tableaux des travaux par catégorie
+    const objetOnly = data.filter(element => element.categoryId === 1)
+    console.table(objetOnly)
+    const appartOnly = data.filter(element => element.categoryId === 2)
+    console.table(appartOnly)
+    const hotelOnly = data.filter(element => element.categoryId === 3)
+    console.table(hotelOnly)
 
-        recupPortfolio(data)
-    })
+    // // Gestion du bouton TOUS (les projets)
+    // const filterButtons = document.querySelectorAll(".filter_btn");
+    // const works = document.querySelectorAll(".figure");
 
+    // filterButtons.forEach(button => {
+    //     button.addEventListener("click", () => {
+    //         const filterValue = button.getAttribute("categoryid");
+    //         filterWorks(filterValue);
+    //         console.log("Afficher tous les travaux");
+    //     });
+    // });
+
+    // function filterWorks(filter) {
+    //     works.forEach(figure => {
+    //         const categoryId = figure.getAttribute("id");
+
+    //         if (filter === "0") {
+    //             figure.style.display = "block";
+    //         } else {
+    //             figure.style.display = "none";
+    //         }
+    //     });
+    // }
+
+};
 
 /* FILTRES */
 
-//Gestion dynamique des filtres
-const filters = document.querySelector(".filters")
-
+//Appel à l'API pour récuperer les catégories
 fetch("http://localhost:5678/api/categories")
     .then(reponse => reponse.json())
     .then(data => {
-        console.table(data)
+        console.table(data);
 
+        recupCat(data);
     })
-    
-/* LOGIN */
+    .catch(error => {
+        console.error('Erreur lors de la récupération des données :', error);
+    });
+
+//Déclaration du conteneur des catégories
+const filters = document.querySelector(".filters");
+
+function recupCat(data) {
+
+    for (let i = 0; i < data.length; i++) {
+        // Création de boutons au contenu dynamique
+        const filter = document.createElement("button");
+        // Attribution d'un categoryId
+        filter.setAttribute("categoryId", data[i].id);
+        // Application du style CSS
+        filter.setAttribute("class", "filter_btn");
+        // Insertion d'une catégorie dans chaque bouton
+        filter.innerText = data[i].name;
+        //Association Contenu-Parents
+        filters.appendChild(filter);
+    };
+
+};
+
+/* LOGGED IN FEATURES*/
+
+const authUser = localStorage.getItem("authToken");
+
+if (authUser) {
+    filters.classList.add("hidden");
+    const worksTitle = document.querySelector(".works_title");
+    worksTitle.classList.add("logged_margin");
+    const banner = document.querySelector(".edit_banner");
+    banner.classList.remove("hidden");
+    const modifiers = document.querySelectorAll(".div_modifier");
+    modifiers.forEach(element => {
+        element.classList.remove("hidden");
+    });
+}
+
+
+
 
 /* MODALE */
 
